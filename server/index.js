1a058de7c7c17b1b7529e1a9c217a6683a8f7e4b
Bfrisbyh92 <Bfrisbyh92@gmail.com>
2022-07-22 13:35:45 -0400

    Started setting up my file structure for a MERN stack app

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import Dotenv from 'dotenv';
+Dotenv.config();
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js'
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server Running on port: ${PORT}`)
+})
+})
+.catch((err) => {
+    console.error(err);
+})
