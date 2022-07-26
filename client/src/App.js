d1bace2149d2e687c383db547c9346eb8c79964e
Bfrisbyh92 <Bfrisbyh92@gmail.com>
2022-07-25 20:13:06 -0400

    Setting up the build to use material-uis makeStyles hook for each pages CSS. Added a logo and header.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
+import { 
+    Container, AppBar,
+    Typography, Grow,
+    Grid, 
+    } from '@material-ui/core';
+    
+import Posts from './components/Posts/Posts';
+import Form  from './components/Form/Form';
+import GoodTimes from './images/GoodTimes.png';
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      Working
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          GoodTimes
+        </Typography>
+        <img className={classes.image} src={GoodTimes} alt="goodtimes" height="80" width="80"  />
+      </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Post from './Post/Post';
+import useStyles from './styles';
+
+const Posts = () => {
+    const classes = useStyles();
+  return (
+    <>
+    Posts
+    <Post />
+    <Post />
+    </>
+  )
+}
+
+export default Posts
