9321b879934229d5c22a74fbb07ee0bc2b8ea1c2
Bfrisbyh92 <Bfrisbyh92@gmail.com>
2022-08-01 19:17:53 -0400

    Broke something trying to implement authorization. Will fix it tommorow. I added auth component and began setting up the form to register and sign in a user. I need users in order to limit likes, add friends, follow other people, etc.

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,8 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import Icon from './Icon';
+import { signin, signup } from '../../actions/auth';
+import { AUTH } from '../../constants/actionTypes';
+import useStyles from './styles';
+import Input from './input';
+
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' };
 
 const Auth = () => {
+  const [form, setForm] = useState(initialState);
+  const [isSignup, setIsSignup] = useState(false);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowPassword = () => setShowPassword(!showPassword);
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+  
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+
+    if (isSignup) {
+      dispatch(signup(form, navigate));
+    } else {
+      dispatch(signin(form, navigate));
+    }
+  };
+
+  const switchMode = () => {
+    setForm(initialState);
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    setShowPassword(false);
+  };
+
+
   return (
-    <div>Auth</div>
+    <Container element='main' maxWidth='xs'>
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant='h5'>
+          {isSignup ? 'Sign up' : 'Sign In'}
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {
+              isSignup && (
+                <>
+                  <Input
+                    name='firstName' 
+                    label='First Name' 
+                    handleChange={handleChange}
+                    autoFocus={true}
+                    half
+                    />
+                    <Input 
+                    name='lastName' 
+                    label='Last Name' 
+                    handleChange={handleChange}
+                    autoFocus={true}
+                    half
+                    />
+                    
+                </>
+              )
+            }
+            <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+            <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+            { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+            { isSignup ? 'Sign Up' : 'Sign In' }
+          </Button>
+        </form>
+      </Paper>
+    </Container>
   )
 }
 
